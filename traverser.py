2eaca1e8ec63104aa852ac78e63df255c2399bac
Praveen Mathew <praveen.mathew92@outlook.com>
2020-11-03 23:39:45 +0530

    Attach another db

--- traverser.py
+++ traverser.py
@@ -3,19 +3,20 @@ from datetime import datetime
 from datetime import timedelta
 
 try:
-    conn = sqlite3.connect('LifeMap_GS1.db')
+    conn = sqlite3.connect('LifeMap_GS4.db')
 except EnvironmentError as e:
     print(e)
 
-date = '20110813113014SAT'
 date_format = "%Y%m%d%H%M%S%a"
-date_seven_days_ago = (datetime.strptime(date, date_format) + timedelta(days=-7)).strftime(date_format)
 
-print(date_seven_days_ago)
+date = '20120813113014SAT'
+date_seven_days_ago = (datetime.strptime(date, date_format) + timedelta(days=-1000)).strftime(date_format)
+
 cur = conn.cursor()
+cur.execute('ATTACH DATABASE "LifeMap_GS2.db" AS GS2')
 
-query = f'SELECT _time_location, _latitude, _longitude FROM locationTable WHERE _time_location <= \"{date}\" AND _time_location >= \"{date_seven_days_ago}\" ;'
-print(query)
+query = f'SELECT main.locationTable._time_location, main.locationTable._latitude, main.locationTable._longitude, GS2.locationTable._latitude, GS2.locationTable._longitude FROM main.locationTable JOIN GS2.locationTable ON main.locationTable._time_location = GS2.locationTable._time_location WHERE main.locationTable._time_location <= \"{date}\" AND main.locationTable._time_location >= \"{date_seven_days_ago}\" ;'
+
 cur.execute(query)
 
 for row in cur.fetchall():
