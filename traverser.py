8fc18d7533a4bb2bc8c60a27bcc74b980365c05b
Praveen Mathew <praveen.mathew92@outlook.com>
2020-11-04 00:31:07 +0530

    Attach multiple database

--- traverser.py
+++ traverser.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 from geopy.distance import distance
 
 try:
-    conn = sqlite3.connect('LifeMap_GS4.db')
+    conn = sqlite3.connect(':memory:')
 except EnvironmentError as e:
     print(e)
 
@@ -16,9 +16,21 @@ date = '20120813113014SAT'
 date_seven_days_ago = (datetime.strptime(date, date_format) + timedelta(days=-1000)).strftime(date_format)
 
 cur = conn.cursor()
-cur.execute('ATTACH DATABASE "LifeMap_GS2.db" AS GS2')
 
-query = f'SELECT main.locationTable._time_location, main.locationTable._latitude, main.locationTable._longitude, GS2.locationTable._latitude, GS2.locationTable._longitude FROM main.locationTable JOIN GS2.locationTable ON main.locationTable._time_location = GS2.locationTable._time_location WHERE main.locationTable._time_location <= \"{date}\" AND main.locationTable._time_location >= \"{date_seven_days_ago}\" ;'
+databases = [
+    { 'id' : 1, 'filename' : "LifeMap_GS1.db" },
+    { 'id' : 2, 'filename' : "LifeMap_GS2.db" },
+]
+
+def attach(databases):
+    for database in databases:
+        subject_id = database['id']
+        filename = database['filename']
+        cur.execute(f'ATTACH DATABASE \"{filename}\" AS GS{subject_id}')
+
+attach(databases = databases)
+
+query = f'SELECT GS1.locationTable._time_location, GS1.locationTable._latitude, GS1.locationTable._longitude, GS2.locationTable._latitude, GS2.locationTable._longitude FROM GS1.locationTable JOIN GS2.locationTable ON GS1.locationTable._time_location = GS2.locationTable._time_location WHERE GS1.locationTable._time_location <= \"{date}\" AND GS1.locationTable._time_location >= \"{date_seven_days_ago}\" ;'
 
 cur.execute(query)
 
