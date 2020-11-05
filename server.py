e622f5cf9ca4ade8451082bfeb58619743d4f225
Praveen Mathew <praveen.mathew92@outlook.com>
2020-11-05 20:42:19 +0530

    Add viretual env

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_file
+from markupsafe import escape
+from traverser import contactgraph, as_txt_file
+
+app = Flask(__name__)
+
+@app.route('/<int:subject_id>/<string:date>')
+def hello_world(subject_id, date):
+    try:
+        response_file = open(as_txt_file(contactgraph(subject_id, date)).name, 'r')
+        return send_file(response_file, as_attachment=True, attachment_filename='adjacency_matrix.txt')
+    except Exception as e:
+        return str(e)
--- traverser.py
+++ traverser.py
@@ -5,13 +5,13 @@ from datetime import timedelta
 from geopy.distance import distance
 
 try:
-    conn = sqlite3.connect(':memory:')
+    conn = sqlite3.connect(':memory:', check_same_thread=False)
 except EnvironmentError as e:
     print(e)
 
 date_format = "%Y%m%d%H%M%S%a"
 PRECISION = math.pow(10, -6)
-INTERVAL = 1000
+INTERVAL = 7
 THRESHOLD = 5
 
 date = '2011114113014MON'
@@ -86,6 +86,18 @@ def contactgraph(subject_id, date):
 # query = f'SELECT GS1.locationTable._time_location, GS1.locationTable._latitude, GS1.locationTable._longitude, GS2.locationTable._latitude, GS2.locationTable._longitude FROM GS1.locationTable JOIN GS2.locationTable ON GS1.locationTable._time_location = GS2.locationTable._time_location WHERE GS1.locationTable._time_location <= \"{date}\" AND GS1.locationTable._time_location >= \"{date_seven_days_ago}\" ;'
 
 # cur.execute(query)
+
+def as_txt_file(contact_graph_edges):
+    file = open('contact_graph.txt', 'w+')
+    for x in range(1, 12):
+        for y in range(1, 12):
+            if((x, y) in row or (y, x) in row):
+                file.write('1 ')
+            else:
+                file.write('0 ')
+        file.write('\n')
+    file.close()
+    return file
 
 for i in range(1, 12):
     print("SUBJECT: ", i)
